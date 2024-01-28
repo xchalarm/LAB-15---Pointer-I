7b0bb95a8f3a9330a92d89922325571a9a99526e
xchalarm <tharana4671@gmail.com>
2024-01-28 11:56:29 +0700

    done

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,20 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a, int &b, int &c, int &d){
+	int dummy1[4] = {a, b, c, d}, dummy2[4];
+	for (int i = 0; i < 4; i++){
+		dummy2[i] = rand()%4;
+		for (int j = 0; j < i; j++){
+			if (dummy2[i] == dummy2[j]){
+				i--;
+				break;
+			}
+		}
+	}
+	a = dummy1[dummy2[0]];
+	b = dummy1[dummy2[1]];
+	c = dummy1[dummy2[2]];
+	d = dummy1[dummy2[3]];	
+}
